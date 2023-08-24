2de55a9929470ebeecbc7a1d4318d5a401f7ab8f
ramdhannassyirah <121411843+ramdhannassyirah@users.noreply.github.com>
2023-08-24 17:10:09 +0700

    Add files via upload

---
+++ components/FooterWeb.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <div class="container">
+      <span class="navbar-text"> &copy; 2022 Taskist. All right reserved </span>
+    </div>
+  </nav>
+</template>
---
+++ components/HeaderWeb.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/contact">Contact</nuxt-link>
+      <nuxt-link to="/product">Product</nuxt-link>
+    </nav>
+  </div>
+</template>
